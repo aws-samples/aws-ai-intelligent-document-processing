1a964301f1cda4ab73ac09e495eaa2f894137bc1
Vijay Prince <vjprince@amazon.com>
2024-07-12 11:59:52 -0600

    Update script

--- .codecatalyst/solutionid_validator.sh
+++ .codecatalyst/solutionid_validator.sh
@@ -7,6 +7,7 @@ echo "grep -nr --exclude-dir='.codecatalyst' "${SOLUTIONID}" ./.."
 result=$(grep -nr --exclude-dir='.codecatalyst' "${SOLUTIONID}" ./..)
 if [ $? -eq 0 ] 
 then
+  echo "Solution ID ${SOLUTIONID} found\n"
   echo "$result"
   exit 0
 else
