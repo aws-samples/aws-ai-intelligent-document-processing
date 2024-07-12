94d2c939e3a682ad1effdc6a183f3ae1e7c74b6b
Vijay Prince <vjprince@amazon.com>
2024-07-12 11:56:12 -0600

    Fix Solution ID code

--- .codecatalyst/solutionid_validator.sh
+++ .codecatalyst/solutionid_validator.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-set -e
-
-SOLUTIONID="SO9052"
+#set -e
+export SOLUTIONID="SO9052"
 
 echo "grep -nr --exclude-dir='.codecatalyst' "${SOLUTIONID}" ./.."
 result=$(grep -nr --exclude-dir='.codecatalyst' "${SOLUTIONID}" ./..)
 if [ $? -eq 0 ] 
 then
   echo "$result"
+  exit 0
 else
-  echo 'Solution ID "${SOLUTIONID}" not found'
+  echo "Solution ID ${SOLUTIONID} not found"
+  exit 1
 fi
 
 export result
