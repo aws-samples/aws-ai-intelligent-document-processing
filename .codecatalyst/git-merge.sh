4f0d93194eac3b480904d31f35299a81e16fdff7
Vijay Prince <vjprince@amazon.com>
2024-07-16 16:41:11 -0600

    Git merge conflict

---
+++ .codecatalyst/git-merge.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+USER_NAME="Vijay Prince"
+EMAIL="vjprince@amazon.com"
+
+git config --list
+git remote add upstream https://github.com/vprince1/aws-ai-intelligent-document-processing.git
+git fetch upstream
+git -c "user.name=Vijay Prince" -c "user.email=vjprince@amazon.com" merge upstream/main --allow-unrelated-histories
+
+echo "git -c "user.name=${USER_NAME}" -c "user.email=${EMAIL}" merge upstream/main --allow-unrelated-histories"
+result=$(git -c "user.name=${USER_NAME}" -c "user.email=${EMAIL}" merge upstream/main --allow-unrelated-histories)
+if [ $? -eq 0 ]
+then
+  echo "Merge Success"
+  echo "$result"
+  #git push
+  exit 0
+else
+  echo "Merge Failed"
+  git diff
+  exit 1
+fi
+
